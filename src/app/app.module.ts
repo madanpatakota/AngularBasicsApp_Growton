c4054556b1468263a747739bf4a0fb0beb7aacd8
Madn <madan.patakota@gmail.com>
2024-09-17 20:22:28 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompOne } from './comp-one.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompOne
   ],
   imports: [
     BrowserModule,
---
+++ src/app/comp-one.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    'selector' : 'app-comp-one',                //name
+    'template' : `<h1>Hello world</h1>`,        //html 
+    'styles'   : [`h1{background-color:red}`]   //css
+})
+
+//buisnesslogic
+export class CompOne{
+
+}
